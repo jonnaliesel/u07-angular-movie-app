1f414dd57ba4a47a1f31b16739f64a98a3ff502a
Jonna <jonnaliesel@gmail.com>
2020-03-11 17:14:02 +0100

    Add posibility to search for title in dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule }    from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -24,6 +25,7 @@ import { MovieSearchComponent } from './movie-search/movie-search.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     HttpClientModule,
     HttpClientInMemoryWebApiModule.forRoot(
--- src/app/movie-search/movie-search.component.ts
+++ src/app/movie-search/movie-search.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+
+import {
+  debounceTime, distinctUntilChanged, switchMap
+} from 'rxjs/operators';
+
+import { Movie } from '../movie';
+import { MovieService } from '../movie.service';
 
 @Component({
   selector: 'app-movie-search',
@@ -7,9 +15,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieSearchComponent implements OnInit {
 
-  constructor() { }
+  movies$: Observable<Movie[]>;
+  private searchTerms = new Subject<string>();
 
-  ngOnInit(): void {
+  constructor(private movieService: MovieService) { }
+
+  search(term: string): void {
+    this.searchTerms.next(term);
   }
 
+  ngOnInit(): void {
+    this.movies$ = this.searchTerms.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((term: string) => this.movieService.searchMovies(term))
+    );
+  }
 }
