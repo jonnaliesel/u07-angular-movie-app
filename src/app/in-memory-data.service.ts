237bdfacb796935ed63fdb8e511e7d8a5510ba13
Jonna <jonnaliesel@gmail.com>
2020-03-11 16:14:21 +0100

    Change from mock-data to web-api-service
    -Add InMemoryService
    -Import httpClient

---
+++ src/app/in-memory-data.service.ts
@@ -0,0 +1,31 @@
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Movie } from './movie';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class InMemoryDataService implements InMemoryDbService {
+  createDb() {
+    const movies = [
+      { id: 11, title: 'Parasit', poster: 'https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SY1000_CR0,0,674,1000_AL_.jpg', year: 2019, rating: 8.6, description: `All unemployed, Ki-taek's family takes peculiar interest in the wealthy and glamorous Parks for their livelihood until they get entangled in an unexpected incident.` },
+      { id: 12, title: 'The invisible man', poster: 'https://m.media-amazon.com/images/M/MV5BZjFhM2I4ZDYtZWMwNC00NTYzLWE3MDgtNjgxYmM3ZWMxYmVmXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_SY1000_CR0,0,631,1000_AL_.jpg', year: 2020, rating: 7.6, description: 'description' },
+      { id: 13, title: 'Parasit', poster: 'https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SY1000_CR0,0,674,1000_AL_.jpg', year: 2019, rating: 8.6, description: 'description' },
+      { id: 14, title: 'The invisible man', poster: 'https://m.media-amazon.com/images/M/MV5BZjFhM2I4ZDYtZWMwNC00NTYzLWE3MDgtNjgxYmM3ZWMxYmVmXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_SY1000_CR0,0,631,1000_AL_.jpg', year: 2020, rating: 7.6, description: 'description' },
+      { id: 15, title: 'Parasit', poster: 'https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SY1000_CR0,0,674,1000_AL_.jpg', year: 2019, rating: 8.6, description: 'description' },
+      { id: 16, title: 'The invisible man', poster: 'https://m.media-amazon.com/images/M/MV5BZjFhM2I4ZDYtZWMwNC00NTYzLWE3MDgtNjgxYmM3ZWMxYmVmXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_SY1000_CR0,0,631,1000_AL_.jpg', year: 2020, rating: 7.6, description: 'description' },
+      { id: 17, title: 'Parasit', poster: 'https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SY1000_CR0,0,674,1000_AL_.jpg', year: 2019, rating: 8.6, description: 'description' },
+      { id: 18, title: 'The invisible man', poster: 'https://m.media-amazon.com/images/M/MV5BZjFhM2I4ZDYtZWMwNC00NTYzLWE3MDgtNjgxYmM3ZWMxYmVmXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_SY1000_CR0,0,631,1000_AL_.jpg', year: 2020, rating: 7.6, description: 'description' },
+    ];
+    return {movies};
+  }
+
+  // Overrides the genId method to ensure that a hero always has an id.
+  // If the heroes array is empty,
+  // the method below returns the initial number (11).
+  // if the heroes array is not empty, the method below returns the highest
+  // hero id + 1.
+  genId(movies: Movie[]): number {
+    return movies.length > 0 ? Math.max(...movies.map(movie => movie.id)) + 1 : 11;
+  }
+}
